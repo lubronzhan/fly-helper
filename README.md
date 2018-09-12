2d3fdc056b351c4c6c99b58bd5d21733be24661e
lubronzhan <lubronzhan@gmail.com>
2018-09-12 12:13:23 -0700

    Add install script
    Signed-off-by: Lubron Zhan <lzhan@pivotal.io>

--- README.md
+++ README.md
@@ -2,3 +2,14 @@
 
 Repo that store all the helpers for cli `fly`
 
+# How to use
+
+```sh
+if [ ! -d $GOPATH/src/github.com/lubronzhan/fly-helper ]; then
+  mkdir -p $GOPATH/src/github.com/lubronzhan/ && \
+  git clone git@github.com:lubronzhan/fly-helper.git $GOPATH/src/github.com/lubronzhan/ && \
+  cd $GOPATH/src/github.com/lubronzhan/fly-helper
+fi
+
+./install
+```
---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+## Add to bash_profile
+[ -n "$(cat ~/.bash_profile | grep fly_helper)" ] || cat >> ~/.bash_profile << EOF
+## fly-helper
+export FLY_TARGET=pks-releng
+export CONCOURSE_URL=https://pks-releng.ci.cf-app.com/
+[ -f ~/.fly_helper ] || ln $GOPATH/src/github.com/lubronzhan/fly-helper/fly_helper_source_me ~/.fly_helper
+source ~/.fly_helper
+EOF
+
+source ~/.bash_profile
